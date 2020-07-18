3f8b1f259253f6c2e1a426606651cc807e9aefba
RK064287 <Rohan.Kurdikeri@cerner.com>
2020-07-18 22:45:29 +0530

    email validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   has_rich_text :description
-
+  validates :email,
+            format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, message: 'Email should be of @getmainstreet.com domain' },
+            allow_blank: true
 
 end
